4e7f6d2b34e8e424b89458e54d267972e4592ee7
ludoRap33 <ludovicdufaure@gmail.com>
2024-05-04 16:24:15 +0200

    step four

--- assets/script.js
+++ assets/script.js
@@ -15,40 +15,69 @@ const slides = [
 		"image": "slide4.png",
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-	var charArry = userInfo.split('5');
-				for {(var i = 1; i <charArry.length; i++></charArry.length>) 
-				if (i == './assets/images/yellow-book.jpg') 
-				charArry.splice(i, 0, '\n');
-				return charArry.toString();}
+]
 
-console.log('test')
+const arrowLeft = document.querySelector('.arrow_left')
+const arrowRight = document.getElementById('arrow_right')
+const dost = document.querySelector('.dots')
 
-const mot = 'test1'
 
-const chiffre = 2
+let indexSlide = 0
 
-const nombre = 33
 
-console.log(mot)
+// événement au click sur l'élément image avec la classe arrow_left
+arrowLeft.addEventListener('click', () => {
+	// affiche dans la console 'click gauche'
+	console.log('click gauche')
 
-console.log(chiffre)
+	indexSlide = indexSlide - 1
+	
+	if (indexSlide === - 1){
+		indexSlide = slides.length - 1
+	}
 
-console.log(nombre)
+	updateSlide()
 
-const addition = chiffre + nombre
-
-console.log(addition)
-
-console.log(slides)
-
-slides.forEach((item, index) => {
-	console.log("image:", item.image)
-	console.log("tagLine:", item.tagLine)
-	console.log('index:', index)
 })
 
-for (let i = 0; i < slides.length; i++) {
-	console.log("image:", slides[i].image)
-	console.log("tagLine:", slides[i].tagLine)
-	console.log('index:', i)
+// événement qui se déclenche au click sur l'élément image avec l'id arrow_right
+arrowRight.addEventListener('click', () => {
+	console.log('click droit')
+
+	indexSlide = indexSlide + 1
+	
+	if (indexSlide === slides.length - 1){
+		indexSlide = 0
+	}
+
+	updateSlide() 
+
+})
+
+const updateSlide = () => {
+	console.log('indexSlide:', indexSlide)
+
+	dost.innerHTML =
+
+	slides.forEach((item, index) => {
+
+		//console.log("image:", item.image)
+		//console.log("tagLine:", item.tagLine)
+		//console.log('index:', index)
+		
+		const dot = document.createElement('span')
+		dot.setAttribute('class', 'dot')
+		dost.appendChild(dot)
+
+		bannerImg.setAttribute('src', /assets/images/slideshow/$(slides[indexSlide].image))
+		tagLine.innerHTML = slides[indexSlide].tagLine
+
+		if (index === indexSlide) {
+			dot.setAttribute('class , dot dot-selected')
+		}
+	})
 }
+
+updateSlide()
+
+for (let i = 0; i < slides.length; i++)
