dc7bbb0eb26da8b0454725fe2e319349547ed447
ludoRap33 <ludovicdufaure@gmail.com>
2024-05-09 15:31:32 +0200

    step five

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Variable slides qui contient tous les slides du carrousel
 const slides = [
 	{
 		"image": "slide1.jpg",
@@ -17,11 +18,14 @@ const slides = [
 	}
 ]
 
+// les variables pour selectionner les éléments du DOM
 const arrowLeft = document.querySelector('.arrow_left')
 const arrowRight = document.getElementById('arrow_right')
-const dost = document.querySelector('.dots')
+const dots = document.querySelector('.dots')
+const bannerImg = document.querySelector('.banner-img')
+const tagLine = document.querySelector('#banner p')
 
-
+// la variable index du carrousel
 let indexSlide = 0
 
 
@@ -30,12 +34,16 @@ arrowLeft.addEventListener('click', () => {
 	// affiche dans la console 'click gauche'
 	console.log('click gauche')
 
+	// on décrémente la variable de l'index du carrousel pour reculer
 	indexSlide = indexSlide - 1
-	
-	if (indexSlide === - 1){
+
+	// la condition pour faire une boucle infini avec le slider dans le sens inverse
+	// on revient au dernier slide si on est positioné au premier slide
+	if (indexSlide === - 1) {
 		indexSlide = slides.length - 1
 	}
 
+	// on met à jour le carrousel en appelant la fonction updateSlide()
 	updateSlide()
 
 })
@@ -44,41 +52,49 @@ arrowLeft.addEventListener('click', () => {
 arrowRight.addEventListener('click', () => {
 	console.log('click droit')
 
+	// on incrémente la variable de l'index du carrousel pour avancer
 	indexSlide = indexSlide + 1
-	
-	if (indexSlide === slides.length - 1){
+
+	// la condition pour faire une boucle infini avec le slider dans le sens normal
+	// on revient au premier slide si on est positioné sur le slide
+	if (indexSlide === slides.length) {
 		indexSlide = 0
 	}
 
-	updateSlide() 
+	// on met à jour le carrousel
+	updateSlide()
 
 })
 
+// Fonction pour mettre à jour le carrousel (image, tagLine, les dots)
 const updateSlide = () => {
 	console.log('indexSlide:', indexSlide)
 
-	dost.innerHTML =
+	// on efface les dots
+	dots.innerHTML = ''
 
+	// on parcours les slides avec forEach
 	slides.forEach((item, index) => {
 
-		//console.log("image:", item.image)
-		//console.log("tagLine:", item.tagLine)
-		//console.log('index:', index)
-		
+		// on créé un dot dans le HTML
 		const dot = document.createElement('span')
+		// on lui ajout une classe 'dot'
 		dot.setAttribute('class', 'dot')
-		dost.appendChild(dot)
+		// on ajoute 'dot' au container 'dots'
+		dots.appendChild(dot)
 
-		bannerImg.setAttribute('src', /assets/images/slideshow/$(slides[indexSlide].image))
+		// On met à jour l'image du carrousel en fonction du slide actuel
+		bannerImg.setAttribute('src', `/assets/images/slideshow/${slides[indexSlide].image}`)
+		// pareil pour le tagLine
 		tagLine.innerHTML = slides[indexSlide].tagLine
 
+		// on met à jour la position du 'dot'
 		if (index === indexSlide) {
-			dot.setAttribute('class , dot dot-selected')
+			dot.setAttribute('class', 'dot dot_selected')
 		}
 	})
 }
 
+// on lance la première fois la mise à jour du carrousel
 updateSlide()
-
-for (let i = 0; i < slides.length; i++);
 
